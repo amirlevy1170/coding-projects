5c94c871e63ae671a3cc28e9531455758d629054
amirlevy1170 <114910417+amirlevy1170@users.noreply.github.com>
2023-05-31 12:48:57 +0300

    Delete Kmeans directory

--- Kmeans/kmeans.h
+++
@@ -1,8 +0,0 @@
-#ifndef __KMEANS_H__
-#define __KMEANS_H__
-# include <Python.h>
-
-
-double** kmeans_main(int K, int iter, int *vec_idxs, int vec_idxs_size, const char *infile_name, double eps);
-
-#endif
--- Kmeans/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension('kmeans', sources=['kmeansmodule.c'])
-setup(name='kmeans',
-     version='1.0',
-     description='Python wrapper for custom C extension',
-     ext_modules=[module])
